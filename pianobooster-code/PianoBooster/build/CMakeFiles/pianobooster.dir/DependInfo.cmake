3cce20f4293eaf31a8d77cc164b8fd8336b3d3f9
H. P. Ladds <householdwords@gmail.com>
2019-04-27 18:43:21 -0400

    Add files via upload

---
+++ pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/DependInfo.cmake
@@ -0,0 +1,78 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/Bar.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/Bar.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/Cfg.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/Cfg.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/Chord.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/Chord.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/Conductor.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/Conductor.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/Draw.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/Draw.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/GlView.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/GlView.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/GuiKeyboardSetupDialog.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/GuiKeyboardSetupDialog.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/GuiLoopingPopup.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/GuiLoopingPopup.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/GuiMidiSetupDialog.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/GuiMidiSetupDialog.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/GuiPreferencesDialog.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/GuiPreferencesDialog.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/GuiSidePanel.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/GuiSidePanel.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/GuiSongDetailsDialog.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/GuiSongDetailsDialog.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/GuiTopBar.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/GuiTopBar.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/Merge.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/Merge.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/MidiDevice.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/MidiDevice.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/MidiDeviceRt.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/MidiDeviceRt.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/MidiFile.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/MidiFile.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/MidiTrack.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/MidiTrack.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/Notation.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/Notation.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/Piano.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/Piano.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/QtMain.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/QtMain.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/QtWindow.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/QtWindow.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/Rating.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/Rating.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/Score.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/Score.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/Scroll.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/Scroll.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/Settings.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/Settings.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/Song.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/Song.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/StavePosition.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/StavePosition.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/Tempo.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/Tempo.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/TrackList.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/TrackList.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/Util.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/Util.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/moc_GlView.cxx" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/moc_GlView.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/moc_GuiKeyboardSetupDialog.cxx" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/moc_GuiKeyboardSetupDialog.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/moc_GuiLoopingPopup.cxx" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/moc_GuiLoopingPopup.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/moc_GuiMidiSetupDialog.cxx" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/moc_GuiMidiSetupDialog.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/moc_GuiPreferencesDialog.cxx" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/moc_GuiPreferencesDialog.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/moc_GuiSidePanel.cxx" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/moc_GuiSidePanel.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/moc_GuiSongDetailsDialog.cxx" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/moc_GuiSongDetailsDialog.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/moc_GuiTopBar.cxx" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/moc_GuiTopBar.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/moc_QtWindow.cxx" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/moc_QtWindow.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/qrc_application.cxx" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/qrc_application.o"
+  "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/src/rtmidi/RtMidi.cpp" "/home/horkan/working2/pianobooster/pianobooster-code/PianoBooster/build/CMakeFiles/pianobooster.dir/rtmidi/RtMidi.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_XML_LIB"
+  "__LINUX_ALSASEQ__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtOpenGL"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtXml"
+  "/usr/include/qt4/QtCore"
+  "build"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pianobooster-code/PianoBooster/build/translations/CMakeFiles/translations.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/translations"
+  "pianobooster_ja.qm"
+  "pianobooster_es.qm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/translations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
