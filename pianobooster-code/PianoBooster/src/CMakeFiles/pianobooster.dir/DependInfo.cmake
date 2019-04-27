b12de69446accfff7d9c1d4710c43fb6d1338613
H. P. Ladds <householdwords@gmail.com>
2019-04-27 18:48:56 -0400

    Add files via upload

---
+++ pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/DependInfo.cmake
@@ -0,0 +1,77 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/Bar.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/Bar.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/Cfg.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/Cfg.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/Chord.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/Chord.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/Conductor.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/Conductor.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/Draw.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/Draw.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/GlView.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/GlView.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/GuiKeyboardSetupDialog.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/GuiKeyboardSetupDialog.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/GuiLoopingPopup.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/GuiLoopingPopup.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/GuiMidiSetupDialog.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/GuiMidiSetupDialog.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/GuiPreferencesDialog.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/GuiPreferencesDialog.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/GuiSidePanel.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/GuiSidePanel.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/GuiSongDetailsDialog.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/GuiSongDetailsDialog.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/GuiTopBar.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/GuiTopBar.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/Merge.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/Merge.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/MidiDevice.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/MidiDevice.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/MidiDeviceRt.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/MidiDeviceRt.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/MidiFile.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/MidiFile.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/MidiTrack.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/MidiTrack.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/Notation.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/Notation.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/Piano.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/Piano.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/QtMain.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/QtMain.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/QtWindow.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/QtWindow.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/Rating.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/Rating.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/Score.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/Score.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/Scroll.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/Scroll.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/Settings.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/Settings.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/Song.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/Song.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/StavePosition.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/StavePosition.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/Tempo.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/Tempo.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/TrackList.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/TrackList.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/Util.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/Util.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/moc_GlView.cxx" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/moc_GlView.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/moc_GuiKeyboardSetupDialog.cxx" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/moc_GuiKeyboardSetupDialog.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/moc_GuiLoopingPopup.cxx" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/moc_GuiLoopingPopup.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/moc_GuiMidiSetupDialog.cxx" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/moc_GuiMidiSetupDialog.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/moc_GuiPreferencesDialog.cxx" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/moc_GuiPreferencesDialog.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/moc_GuiSidePanel.cxx" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/moc_GuiSidePanel.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/moc_GuiSongDetailsDialog.cxx" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/moc_GuiSongDetailsDialog.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/moc_GuiTopBar.cxx" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/moc_GuiTopBar.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/moc_QtWindow.cxx" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/moc_QtWindow.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/qrc_application.cxx" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/qrc_application.o"
+  "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/rtmidi/RtMidi.cpp" "/home/hpladds/Documents/Makers/Projects/PiBoo/pianobooster/pianobooster-code/PianoBooster/src/CMakeFiles/pianobooster.dir/rtmidi/RtMidi.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_XML_LIB"
+  "__LINUX_ALSASEQ__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtOpenGL"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtXml"
+  "/usr/include/qt4/QtCore"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
